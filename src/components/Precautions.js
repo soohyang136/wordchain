f07656a733fb3a21118a70584140f3511b7b3363
soohyang136 <jsh515228@gmail.com>
2022-07-04 09:04:44 +0900

    두음법칙

--- src/components/Precautions.js
+++ src/components/Precautions.js
@@ -4,7 +4,7 @@ import '../App.css';
 export default function Precautions(){
     return (<>
         <p className="preca">*주의사항: 명사만 검색 가능</p>
-        <p>실행이 되지 않는다면?? 클릭하여 확장프로그램 추가 <a href="https://chrome.google.com/webstore/detail/allow-cors-access-control/lhobafahddgcelffkeicbaginigeejlf/related?hl=ko">이곳</a></p>
+        <p>실행이 되지 않는다면?? <a href="https://chrome.google.com/webstore/detail/allow-cors-access-control/lhobafahddgcelffkeicbaginigeejlf/related?hl=ko">이곳</a> 클릭하여 확장프로그램 추가 </p>
         </>
     )
 }
--- src/components/Word.js
+++ src/components/Word.js
@@ -5,6 +5,30 @@ import '../App.css';
 export default function Word(){    
     const [word, setWord] = useState([]);
     const [print, setPrint] = useState([]);
+    const change = {
+        "라":"나","락":"낙","란":"난","랄":"날",
+        "람":"남","랍":"납","랏":"낫","랑":"낭",
+        "략":"약","냑":"약","량":"양","냥":"양",
+        "렁":"넝","려":"여","녀":"여","력":"역",
+        "녁":"역","련":"연","년":"연","렬":"열",
+        "녈":"열","렴":"염","념":"염","렵":"엽",
+        "령":"영","녕":"영","로":"노","록":"녹",
+        "론":"논","롤":"놀","롬":"놈","롭":"놉",
+        "롯":"놋","롱":"농","뢰":"뇌","료":"요",
+        "뇨":"요","룡":"용","뇽":"용","루":"누",
+        "룩":"눅","룬":"눈","룰":"눌","룸":"눔",
+        "룻":"눗","룽":"눙","류":"유","뉴":"유",
+        "륙":"육","뉵":"육","륜":"윤","률":"율",
+        "륭":"융","르":"느","륵":"늑","른":"는",
+        "를":"늘","름":"늠","릅":"늡","릇":"늣",
+        "릉":"능","래":"내","랙":"낵","랜":"낸",
+        "랠":"낼","램":"냄","랩":"냅","랫":"냇",
+        "랭":"냉","례":"예","녜":"예","리":"이",
+        "니":"이","린":"인","닌":"인","릴":"일",
+        "닐":"일","림":"임","님":"임","립":"입",
+        "닙":"입","릿":"잇","닛":"잇","링":"잉",
+        "닝":"잉"
+    }
     const inputRef = useRef(null);
     function rem(arr){
         arr.pop();
@@ -13,7 +37,7 @@ export default function Word(){
 
     useEffect(() => {
          async function fetchData(){
-            const response = await axios.get("https://opendict.korean.go.kr/api/search?certkey_no=4082&key=7C0D6B2BE5A04149309C92F60A3564B7&target_type=search&req_type=json&part=word&q="+ word[word.length - 1] + "&sort=dict&start=1&num=10&advanced=y&pos=1");
+            const response = await axios.get("https://opendict.korean.go.kr/api/search?certkey_no=4082&key=7C0D6B2BE5A04149309C92F60A3564B7&target_type=search&req_type=json&part=word&q="+ word[word.length - 1] + "&sort=popular&start=1&num=10&advanced=y&pos=1");
             if(response.data.channel.total === 0) {
                 const arr = rem(word);
                 setWord(prev => arr);
@@ -38,7 +62,8 @@ export default function Word(){
         else{
             const lastword = word[word.length - 1];
             const lastspelling = lastword.slice(-1)[0];
-            if(lastspelling === e.target.value[0]){
+            console.log(change[lastspelling]);
+            if(lastspelling === e.target.value[0] || change[lastspelling] !== ""){
                 let flag = true;
                 for(let i = 0; i < word.length; i++){
                     if(word[i] === e.target.value){
@@ -75,9 +100,9 @@ export default function Word(){
                 </div>
                 {print.map((w, index) => {
                     return (
-                        <div className='print'>
-                            <h2 key={index}>{w.wording}</h2>
-                            <h4 key={index}>{w.define}</h4>
+                        <div className='print' key={index}>
+                            <h2 >{w.wording}</h2>
+                            <h4 >{w.define}</h4>
                         </div>
                     )
                 })
